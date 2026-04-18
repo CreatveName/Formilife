db263f52d60210240123ee47db67c86faafa5d7a
CreatveName <trentku10@gmail.com>
2026-04-18 01:00:44 -0700

    Formicary texture update

--- Formilife/Assets/Scripts/Background/Formicary.cs
+++ Formilife/Assets/Scripts/Background/Formicary.cs
@@ -4,18 +4,19 @@ public class Formicary : MonoBehaviour
 {
     public Texture2D logTexture;
     public Texture2D leafTexture;
+    public Renderer wallR;
 
 
 
     void Start()
     {
-        Transform formicaryWalls = transform.Find("Assets/3D Models/Formicary.fbx");
-        ChangeTexture(formicaryWalls, logTexture);
+        //Transform formicaryWalls = transform.Find("Assets/3D Models/Formicary.fbx");
+        ChangeTexture(wallR, logTexture);
     }
 
-    void ChangeTexture(Transform part, Texture2D newTexture)
+    void ChangeTexture(Renderer part, Texture2D newTexture)
     {
-        Renderer myRenderer = part.GetComponent<Renderer>();
-        myRenderer.material.mainTexture = newTexture;
+        //Renderer myRenderer = part.GetComponent<Renderer>();
+        wallR.material.mainTexture = newTexture;
     }
 }
