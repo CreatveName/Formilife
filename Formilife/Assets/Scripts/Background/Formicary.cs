0bd3c0f2279eadb557c8843847894b201c08398e
kkstanford-zip <kkstanfo@uci.edu>
2026-04-19 00:24:52 -0700

    make background table & flip formicary

--- Formilife/Assets/Scripts/Background/Formicary.cs
+++ Formilife/Assets/Scripts/Background/Formicary.cs
@@ -4,7 +4,11 @@ public class Formicary : MonoBehaviour
 {
     public Texture2D logTexture;
     public Texture2D leafTexture;
+    public Texture2D lightWoodTexture;
+    public Texture2D darkWoodTexture;
+
     public Renderer wallR;
+    public Renderer backgroundPlane;
 
 
 
@@ -12,6 +16,7 @@ public class Formicary : MonoBehaviour
     {
         //Transform formicaryWalls = transform.Find("Assets/3D Models/Formicary.fbx");
         ChangeTexture(wallR, logTexture);
+        ChangeTexture(backgroundPlane, lightWoodTexture);
     }
 
     void ChangeTexture(Renderer part, Texture2D newTexture)
